b3e5f544df3d51128e51fa35131a2f04e96dbed1
Christine Movius <christine.movius@gmail.com>
2016-02-22 12:04:25 -0500

    Created function that displays frontVal text on screen.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//DOM Variables
+var cardText = document.querySelector("#cardText");
+
 //Create Card container
 function card(front, back){
     this.frontVal = front;
@@ -12,6 +15,15 @@ game = {
   cardAdd: function(front, back) {
     this.cards.push(new card(front, back));
     console.log(this.cards);
+  },
+  //Create a function that displays cards when page loads.
+  showCard : function() {
+    for (var i = 0; i < game.cards.length; i++) {
+    var newDiv = document.createElement("div");
+    cardText.appendChild(newDiv);
+    console.log(newDiv);
+    newDiv.innerText= (game.cards[i].frontVal);
+    }
   }
 };
 
@@ -19,3 +31,4 @@ game = {
 game.cardAdd("How are you?", "Apa khabar?");
 game.cardAdd("My name is _.", "Name saya _.");
 game.cardAdd("Have you already eaten?", "Sudah makan?");
+game.showCard();
