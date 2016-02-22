5dc461200130f83b7e5d2f2cda1244a8a7c98bad
Christine Movius <christine.movius@gmail.com>
2016-02-22 11:27:15 -0500

    Created small default deck of 3 flashcards.

---
+++ script.js
@@ -0,0 +1,21 @@
+//Create Card container
+function card(front, back){
+    this.frontVal = front;
+    this.backVal = back;
+}
+
+// Create a Game Object
+game = {
+  // The cards are an empty array to start.
+  cards: [],
+  // Create a function that adds the front and back values to the card array.
+  cardAdd: function(front, back) {
+    this.cards.push(new card(front, back));
+    console.log(this.cards);
+  }
+};
+
+//Default Cards
+game.cardAdd("How are you?", "Apa khabar?");
+game.cardAdd("My name is _.", "Name saya _.");
+game.cardAdd("Have you already eaten?", "Sudah makan?");
