0882a1786290aa1af4acea974fc56d616990c205
Christine Movius <christine.movius@gmail.com>
2016-02-22 16:48:46 -0500

    Started playing with userAdd function to get user input for flashcards. Function creates an additional card, but card is undefined. Need to attach frontVal and backVal to user input.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //DOM Variables
 var cardText = document.querySelector("#cardText");
-var englishVal = document.body.querySelector(".frontVal");
 
 //Create Card container
 function card(front, back){
@@ -8,13 +7,19 @@ function card(front, back){
     this.backVal = back;
 }
 
-// Create a Game Object
+//Create a Game Object
 game = {
   // The cards are an empty array to start.
   cards: [],
-  // Create a function that adds the front and back values to the card array.
+  //Create a function that adds the front and back values to the card array.
   cardAdd: function(front, back) {
     this.cards.push(new card(front, back));
+    console.log(this.cards);
+  },
+  //Create function that logs user inputs of English and Malay.
+  userAdd: function() {
+    var userWord = prompt("Please provide a word in English and a word in Malay. Please separate the words with a space.");
+    game.cards.push(userWord.split(" "));
     console.log(this.cards);
   },
   //Create a function that displays English side of the cards when page loads.
@@ -28,11 +33,17 @@ game = {
     newDiv.innerText = (this.cards[i].frontVal);
     }
   },
-  //Create a function that switches English text to the corresponding Malay text when text is clicked.
+  //Create a function that switches English text to the corresponding Malay text when text mouseover.
   translate: function(event) {
     event.preventDefault();
     var thisCard = game.cards[event.target.dataset.cardIndex];
     event.target.innerText = thisCard.backVal;
+  },
+  //Create a function that switches Malay text back to the corresponding English text when text mouseout.
+  revert: function(event) {
+    event.preventDefault();
+    var frontCard = game.cards[event.target.dataset.cardIndex];
+    event.target.innerText = frontCard.frontVal;
   }
 };
 
@@ -40,5 +51,7 @@ game = {
 game.cardAdd("How are you?", "Apa khabar?");
 game.cardAdd("My name is _________.", "Name saya _________.");
 game.cardAdd("Have you already eaten?", "Sudah makan?");
+game.userAdd();
 game.showCard();
-cardText.addEventListener("click", game.translate);
+cardText.addEventListener("mouseover", game.translate);
+cardText.addEventListener("mouseout", game.revert);
